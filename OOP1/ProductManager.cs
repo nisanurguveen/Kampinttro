6a60d6a44fdb3e5dbac6a7be0cdcc509b1bea4cf
nisanurguveen <nisanurguveen@gmail.com>
2021-01-22 16:24:04 +0300

    proje eklendi

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product) //string ad ile aynı şey verdiğimiz değer buraya atanacak demek 
+        {
+            Console.WriteLine(product.ProductName + " Eklendi.");
+        }
+
+        public void update(Product product) 
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+       
+      
+
+
+
+    }
+}
